5b99a53095bbda80e7229242d276905f65cc9d39
Robert Penner <robertpenner@microsoft.com>
2023-11-07 17:58:46 -0500

    feat(motion): Tree has a first stab at expand/collapse. Needs bug fixing and refinement.

--- packages/react-components/react-tree/src/components/Tree/renderTree.tsx
+++ packages/react-components/react-tree/src/components/Tree/renderTree.tsx
@@ -4,11 +4,67 @@ import { assertSlots } from '@fluentui/react-utilities';
 import type { TreeContextValues, TreeSlots, TreeState } from '../Tree/Tree.types';
 import { TreeProvider } from '../TreeProvider';
 
+import { Transition } from 'react-transition-group';
+import { useRef, CSSProperties, useEffect } from 'react';
+
+type TransitionState = 'entering' | 'entered' | 'exiting' | 'exited' | 'unmounted';
+const inOrOutByState: Record<TransitionState, 'in' | 'out'> = {
+  entering: 'in',
+  entered: 'in',
+  exiting: 'out',
+  exited: 'out',
+  unmounted: 'out',
+};
+
+const duration = 200;
+
+const expandTransitionConfig = {
+  transitionProperty: 'opacity, max-height',
+  // in: { opacity: 1, maxHeight: '200px' },
+  in: { opacity: 1 },
+  out: { opacity: 0 },
+  common: { transitionDuration: `${duration}ms`, transitionTimingFunction: 'ease-out' },
+};
+
 export const renderTree_unstable = (state: TreeState, contextValues: TreeContextValues) => {
   assertSlots<TreeSlots>(state);
+  const nodeRef = useRef<HTMLElement>(null);
+  const baseStyle: CSSProperties = {
+    width: 'fit-content',
+    height: 'fit-content',
+  };
+  const config = expandTransitionConfig;
+
+  useEffect(() => {
+    const element = nodeRef.current;
+    if (!element) {
+      return;
+    }
+    if (state.open) {
+      element.style.maxHeight = element.scrollHeight + 'px';
+    } else {
+      element.style.maxHeight = '0px';
+    }
+  }, [state.open]);
   return (
     <TreeProvider value={contextValues.tree}>
-      {state.open && <state.root>{state.root.children}</state.root>}
+      {/* original show/hide without transition */}
+      {/* {state.open && <state.root>{state.root.children}</state.root>} */}
+      <Transition nodeRef={nodeRef} in={state.open} timeout={duration}>
+        {transitionState => (
+          <div
+            ref={nodeRef}
+            style={{
+              ...baseStyle,
+              transitionProperty: config.transitionProperty,
+              ...config.common,
+              ...config[inOrOutByState[transitionState]],
+            }}
+          >
+            <state.root>{state.root.children}</state.root>
+          </div>
+        )}
+      </Transition>
     </TreeProvider>
   );
 };
