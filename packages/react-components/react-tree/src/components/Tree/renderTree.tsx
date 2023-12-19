c6dcaf32ca2a8af4287e1867503414025e898e66
Robert Penner <robert@robertpenner.com>
2023-12-19 06:12:06 +0000

    feat(Accordion): apply Collapse motion molecule

--- packages/react-components/react-tree/src/components/Tree/renderTree.tsx
+++ packages/react-components/react-tree/src/components/Tree/renderTree.tsx
@@ -13,7 +13,7 @@ export const renderTree_unstable = (state: TreeState, contextValues: TreeContext
       {/* original show/hide without transition */}
       {/* {state.open && <state.root>{state.root.children}</state.root>} */}
 
-      {/* Wrap the child content in a Collapse transition which manages show/hide */}
+      {/* Wrap child content in a Collapse transition which manages show/hide */}
       <Collapse visible={state.open}>
         <state.root>{state.root.children}</state.root>
       </Collapse>
