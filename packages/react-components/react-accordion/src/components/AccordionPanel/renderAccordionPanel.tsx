c571e392ccbbbfbee5e334779ee09cc0768a1431
Robert Penner <robert@robertpenner.com>
2024-04-04 18:29:13 +0000

    feat(motion): start on injecting motion into Accordion

--- packages/react-components/react-accordion/src/components/AccordionPanel/renderAccordionPanel.tsx
+++ packages/react-components/react-accordion/src/components/AccordionPanel/renderAccordionPanel.tsx
@@ -1,10 +1,17 @@
 /** @jsxRuntime automatic */
 /** @jsxImportSource @fluentui/react-jsx-runtime */
+// import * as React from 'react';
 
 import { assertSlots } from '@fluentui/react-utilities';
 import type { AccordionPanelState, AccordionPanelSlots } from './AccordionPanel.types';
 // import { Collapse, durations, curves } from '@fluentui/react-motions-preview';
+// import { Collapse, curves } from '@fluentui/react-motions-preview';
 import { Collapse } from '@fluentui/react-motions-preview';
+
+const MotionFC = Collapse;
+
+// const MotionFC: React.FC<{ visible: boolean }> = ({ visible, children, ...otherProps }) =>
+//   Collapse({ visible, children, override: { all: { duration: 200, easing: curves.curveAccelerateMid } } });
 
 // TODO: allow the motion to be injected and enable that from Storybook
 // TODO: unify this open/close duration with the header's chevron rotation duration
@@ -17,6 +24,9 @@ import { Collapse } from '@fluentui/react-motions-preview';
  */
 export const renderAccordionPanel_unstable = (state: AccordionPanelState) => {
   assertSlots<AccordionPanelSlots>(state);
+
+  // TODO: read the motion from the context
+
   // Wrap child content in a Collapse transition which manages show/hide
   return (
     // <Collapse
@@ -27,8 +37,8 @@ export const renderAccordionPanel_unstable = (state: AccordionPanelState) => {
     //     exit: { easing: exitEasing },
     //   }}
     // >
-    <Collapse visible={state.open}>
+    <MotionFC visible={state.open}>
       <state.root>{state.root.children}</state.root>
-    </Collapse>
+    </MotionFC>
   );
 };
