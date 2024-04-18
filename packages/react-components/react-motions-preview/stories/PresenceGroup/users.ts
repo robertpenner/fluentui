ae1e74491929fa29bcd5ca53cb41a6aeef786b59
Robert Penner <robert@robertpenner.com>
2024-04-18 16:15:58 +0000

    refactor(docs): move users fake data to a file (from PresenceGroup story)

---
+++ packages/react-components/react-motions-preview/stories/PresenceGroup/users.ts
@@ -0,0 +1,118 @@
+export const users = [
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/AllanMunger.jpg',
+    name: 'Allan Munger',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/AmandaBrady.jpg',
+    name: 'Amanda Brady',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/AshleyMcCarthy.jpg',
+    name: 'Ashley McCarthy',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/CameronEvans.jpg',
+    name: 'Cameron Evans',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/CarlosSlattery.jpg',
+    name: 'Carlos Slattery',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/CarolePoland.jpg',
+    name: 'Carole Poland',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/CecilFolk.jpg',
+    name: 'Cecil Folk',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/CelesteBurton.jpg',
+    name: 'Celeste Burton',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/CharlotteWaltson.jpg',
+    name: 'Charlotte Waltson',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/ColinBallinger.jpg',
+    name: 'Colin Ballinger',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/DaisyPhillips.jpg',
+    name: 'Daisy Phillips',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/ElliotWoodward.jpg',
+    name: 'Elliot Woodward',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/ElviaAtkins.jpg',
+    name: 'Elvia Atkins',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/ErikNason.jpg',
+    name: 'Erik Nason',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/HenryBrill.jpg',
+    name: 'Henry Brill',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/IsaacFielder.jpg',
+    name: 'Isaac Fielder',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/JohnieMcConnell.jpg',
+    name: 'Johnie McConnell',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/KatLarsson.jpg',
+    name: 'Kat Larsson',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/KatriAthokas.jpg',
+    name: 'Katri Athokas',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/KevinSturgis.jpg',
+    name: 'Kevin Sturgis',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/KristinPatterson.jpg',
+    name: 'Kristin Patterson',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/LydiaBauer.jpg',
+    name: 'Lydia Bauer',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/MauricioAugust.jpg',
+    name: 'Mauricio August',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/MiguelGarcia.jpg',
+    name: 'Miguel Garcia',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/MonaKane.jpg',
+    name: 'Mona Kane',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/RobertTolbert.jpg',
+    name: 'Robert Tolbert',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/RobinCounts.jpg',
+    name: 'Robin Counts',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/TimDeboer.jpg',
+    name: 'Tim Deboer',
+  },
+  {
+    image: 'https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/WandaHoward.jpg',
+    name: 'Wanda Howard',
+  },
+];
