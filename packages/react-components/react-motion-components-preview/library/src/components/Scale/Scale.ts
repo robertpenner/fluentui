291677feb9416cdde484d3f29d4d3e35fd65cc33
Robert Penner <robertpenner@microsoft.com>
2025-03-09 03:03:05 +0000

    refactor(Scale):

--- packages/react-components/react-motion-components-preview/library/src/components/Scale/Scale.ts
+++ packages/react-components/react-motion-components-preview/library/src/components/Scale/Scale.ts
@@ -1,6 +1,7 @@
-import { motionTokens, createPresenceComponent } from '@fluentui/react-motion';
-import { PresenceMotionFnCreator } from '../../types';
+import { motionTokens, createPresenceComponent, PresenceMotionFn } from '@fluentui/react-motion';
+import { EnterExit, PresenceMotionFnCreator } from '../../types';
 import { ScaleRuntimeParams_unstable, ScaleVariantParams_unstable } from './Scale.types';
+import { enterValue, exitValue } from '../../utils/presence-utils';
 
 /** Define a presence motion for scale in/out */
 export const createScalePresence: PresenceMotionFnCreator<ScaleVariantParams_unstable, ScaleRuntimeParams_unstable> =
@@ -34,6 +35,39 @@ export const createScalePresence: PresenceMotionFnCreator<ScaleVariantParams_uns
       exit: { duration: exitDuration, easing: exitEasing, keyframes: exitKeyframes },
     };
   };
+
+const scalePresenceFn: PresenceMotionFn<{
+  duration: EnterExit<number>;
+  easing: EnterExit<string>;
+  animateOpacity?: boolean;
+}> = ({
+  duration = motionTokens.durationNormal,
+  easing = [motionTokens.curveDecelerateMid, motionTokens.curveAccelerateMid],
+  animateOpacity = true,
+}) => {
+  const fromOpacity = animateOpacity ? 0 : 1;
+  const toOpacity = 1;
+  const fromScale = 0.9; // Could be a custom param in the future
+  const toScale = 1;
+
+  const enterKeyframes = [
+    { opacity: fromOpacity, transform: `scale3d(${fromScale}, ${fromScale}, 1)`, visibility: 'visible' },
+    { opacity: toOpacity, transform: `scale3d(${toScale}, ${toScale}, 1)` },
+  ];
+
+  const exitKeyframes = [
+    { opacity: toOpacity, transform: `scale3d(${toScale}, ${toScale}, 1)` },
+    { opacity: fromOpacity, transform: `scale3d(${fromScale}, ${fromScale}, 1)`, visibility: 'hidden' },
+  ];
+  return {
+    enter: {
+      duration: enterValue(duration),
+      easing: enterValue(easing),
+      keyframes: enterKeyframes,
+    },
+    exit: { duration: exitValue(duration), easing: exitValue(easing), keyframes: exitKeyframes },
+  };
+};
 
 /** A React component that applies scale in/out transitions to its children. */
 export const Scale = createPresenceComponent(createScalePresence());
