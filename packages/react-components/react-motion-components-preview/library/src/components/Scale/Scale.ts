67ca56b6c6d2e2e4cc74c9c7b99f93c19f0647d1
Robert Penner <robertpenner@microsoft.com>
2025-03-09 05:27:31 +0000

    refactor(Scale): simplify variant creation

--- packages/react-components/react-motion-components-preview/library/src/components/Scale/Scale.ts
+++ packages/react-components/react-motion-components-preview/library/src/components/Scale/Scale.ts
@@ -1,41 +1,13 @@
-import { motionTokens, createPresenceComponent, PresenceMotionFn } from '@fluentui/react-motion';
-import { EnterExit, PresenceMotionFnCreator } from '../../types';
-import { ScaleRuntimeParams_unstable, ScaleVariantParams_unstable } from './Scale.types';
+import {
+  motionTokens,
+  createPresenceComponent,
+  PresenceMotionFn,
+  EnterExit,
+  createPresenceComponentVariant,
+} from '@fluentui/react-motion';
 import { enterValue, exitValue } from '../../utils/presence-utils';
 
 /** Define a presence motion for scale in/out */
-export const createScalePresence: PresenceMotionFnCreator<ScaleVariantParams_unstable, ScaleRuntimeParams_unstable> =
-  ({
-    enterDuration = motionTokens.durationGentle,
-    enterEasing = motionTokens.curveDecelerateMax,
-    exitDuration = motionTokens.durationNormal,
-    exitEasing = motionTokens.curveAccelerateMax,
-  } = {}) =>
-  ({ animateOpacity = true }) => {
-    const fromOpacity = animateOpacity ? 0 : 1;
-    const toOpacity = 1;
-    const fromScale = 0.9; // Could be a custom param in the future
-    const toScale = 1;
-
-    const enterKeyframes = [
-      { opacity: fromOpacity, transform: `scale3d(${fromScale}, ${fromScale}, 1)`, visibility: 'visible' },
-      { opacity: toOpacity, transform: `scale3d(${toScale}, ${toScale}, 1)` },
-    ];
-
-    const exitKeyframes = [
-      { opacity: toOpacity, transform: `scale3d(${toScale}, ${toScale}, 1)` },
-      { opacity: fromOpacity, transform: `scale3d(${fromScale}, ${fromScale}, 1)`, visibility: 'hidden' },
-    ];
-    return {
-      enter: {
-        duration: enterDuration,
-        easing: enterEasing,
-        keyframes: enterKeyframes,
-      },
-      exit: { duration: exitDuration, easing: exitEasing, keyframes: exitKeyframes },
-    };
-  };
-
 const scalePresenceFn: PresenceMotionFn<{
   duration: EnterExit<number>;
   easing: EnterExit<string>;
@@ -50,6 +22,8 @@ const scalePresenceFn: PresenceMotionFn<{
   const fromScale = 0.9; // Could be a custom param in the future
   const toScale = 1;
 
+  // TODO: use fadeAtom
+  // TODO: make scaleAtom
   const enterKeyframes = [
     { opacity: fromOpacity, transform: `scale3d(${fromScale}, ${fromScale}, 1)`, visibility: 'visible' },
     { opacity: toOpacity, transform: `scale3d(${toScale}, ${toScale}, 1)` },
@@ -59,6 +33,7 @@ const scalePresenceFn: PresenceMotionFn<{
     { opacity: toOpacity, transform: `scale3d(${toScale}, ${toScale}, 1)` },
     { opacity: fromOpacity, transform: `scale3d(${fromScale}, ${fromScale}, 1)`, visibility: 'hidden' },
   ];
+
   return {
     enter: {
       duration: enterValue(duration),
@@ -70,22 +45,14 @@ const scalePresenceFn: PresenceMotionFn<{
 };
 
 /** A React component that applies scale in/out transitions to its children. */
-export const Scale = createPresenceComponent(createScalePresence());
+export const Scale = createPresenceComponent(scalePresenceFn);
 
-export const ScaleSnappy = createPresenceComponent(
-  createScalePresence({
-    enterDuration: motionTokens.durationNormal,
-    enterEasing: motionTokens.curveDecelerateMax,
-    exitDuration: motionTokens.durationFast,
-    exitEasing: motionTokens.curveAccelerateMax,
-  }),
-);
+export const ScaleSnappy = createPresenceComponentVariant(Scale, {
+  duration: motionTokens.durationFast,
+  easing: [motionTokens.curveDecelerateMax, motionTokens.curveAccelerateMax],
+});
 
-export const ScaleRelaxed = createPresenceComponent(
-  createScalePresence({
-    enterDuration: motionTokens.durationSlow,
-    enterEasing: motionTokens.curveDecelerateMax,
-    exitDuration: motionTokens.durationGentle,
-    exitEasing: motionTokens.curveAccelerateMax,
-  }),
-);
+export const ScaleRelaxed = createPresenceComponentVariant(Scale, {
+  duration: motionTokens.durationGentle,
+  easing: [motionTokens.curveDecelerateMid, motionTokens.curveAccelerateMid],
+});
