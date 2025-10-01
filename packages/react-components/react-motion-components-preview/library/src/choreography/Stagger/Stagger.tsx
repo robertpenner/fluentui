e5574491556fb482fc85a9f49886cbe875486fd9
Robert Penner <robertpenner@microsoft.com>
2025-10-01 20:59:38 +0000

    chore: add 'use client' directive to Stagger components for client-side rendering

--- packages/react-components/react-motion-components-preview/library/src/choreography/Stagger/Stagger.tsx
+++ packages/react-components/react-motion-components-preview/library/src/choreography/Stagger/Stagger.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import * as React from 'react';
 import { useStaggerItemsVisibility } from './useStaggerItemsVisibility';
 import {
--- packages/react-components/react-motion-components-preview/library/src/choreography/Stagger/useStaggerItemsVisibility.tsx
+++ packages/react-components/react-motion-components-preview/library/src/choreography/Stagger/useStaggerItemsVisibility.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import * as React from 'react';
 import { useAnimationFrame } from '@fluentui/react-utilities';
 import type { StaggerProps } from './stagger-types';
