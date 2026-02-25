c66fcf7405c625a551563b0f4d6fe9b636820aa3
Robert Penner <robertpenner@microsoft.com>
2026-02-25 21:07:28 +0000

    refactor: update ScaleJeremy story to use Card components for layout

--- packages/react-components/react-motion-components-preview/stories/src/Scale/ScaleJeremy.stories.tsx
+++ packages/react-components/react-motion-components-preview/stories/src/Scale/ScaleJeremy.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import type { JSXElement } from '@fluentui/react-components';
-import { Field, makeStyles, tokens, Switch } from '@fluentui/react-components';
+import { Card, CardHeader, Field, makeStyles, Switch, Text } from '@fluentui/react-components';
 import { ScaleJeremy } from '@fluentui/react-motion-components-preview';
 
 import description from './ScaleJeremy.stories.md';
@@ -14,17 +14,11 @@ const useClasses = makeStyles({
   card: {
     gridArea: 'card',
     padding: '20px',
-    border: `${tokens.strokeWidthThicker} solid ${tokens.colorNeutralForeground3}`,
-    borderRadius: tokens.borderRadiusMedium,
   },
   controls: {
     display: 'flex',
     flexDirection: 'column',
     gridArea: 'controls',
-
-    border: `${tokens.strokeWidthThicker} solid ${tokens.colorNeutralForeground2}`,
-    borderRadius: tokens.borderRadiusMedium,
-    boxShadow: tokens.shadow16,
     padding: '10px',
   },
   field: {
@@ -46,16 +40,23 @@ export const Jeremy = (): JSXElement => {
 
   return (
     <div className={classes.container}>
-      <div className={classes.controls}>
+      <Card className={classes.controls}>
         <Field className={classes.field}>
           <Switch label="Visible" checked={visible} onChange={() => setVisible(v => !v)} />
         </Field>
-      </div>
+      </Card>
 
       <ScaleJeremy visible={visible}>
-        <div className={classes.card}>
+        <Card className={classes.card}>
+          <CardHeader
+            header={
+              <Text as="h3" style={{ margin: 0 }} weight="semibold">
+                Lorem Ipsum
+              </Text>
+            }
+          />
           <LoremIpsum />
-        </div>
+        </Card>
       </ScaleJeremy>
     </div>
   );
